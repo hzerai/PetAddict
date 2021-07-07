c1ef9890c6dbd5be7f2caceccf002c99ed4715f5
sawsansfaihi <85118797+sawsansfaihi@users.noreply.github.com>
2021-07-07 12:35:21 +0100

    update callBacks annotations

--- src/Entity/Found.php
+++ src/Entity/Found.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=FoundRepository::class)
+ * @ORM\Entity(repositoryClass=AdoptionRepository::class) @ORM\HasLifecycleCallbacks
  */
 class Found
 {
--- src/Entity/Lost.php
+++ src/Entity/Lost.php
@@ -7,6 +7,8 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=LostRepository::class)
+ * @ORM\Entity(repositoryClass=AdoptionRepository::class) @ORM\HasLifecycleCallbacks
+
  */
 class Lost
 {
