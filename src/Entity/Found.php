6f057a6bc74666f1589dc9db2d066f6151ac4283
sawsansfaihi <85118797+sawsansfaihi@users.noreply.github.com>
2021-07-10 15:33:50 +0100

    Lost and found services

---
+++ src/Entity/Found.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace App\Entity;
+use DateTime;
+use App\Repository\FoundRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FoundRepository::class)
+ * @ORM\Entity(repositoryClass=AdoptionRepository::class) @ORM\HasLifecycleCallbacks
+
+ */
+class Found
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    private $user;
+    
+    /**
+     * @ORM\OneToOne(targetEntity=Address::class,  cascade={"persist", "remove"})
+     */
+    private $address;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $createdBy;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $updatedBy;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\OneToOne(targetEntity=Animal::class,  cascade={"persist", "remove"})
+     */
+    private $animal;
+
+
+
+    
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?string
+    {
+        return $this->createdBy;
+    }
+
+    public function setCreatedBy(?string $createdBy): self
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedBy(): ?string
+    {
+        return $this->updatedBy;
+    }
+
+    public function setUpdatedBy(?string $updatedBy): self
+    {
+        $this->updatedBy = $updatedBy;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+/** @ORM\PrePersist */
+public function prePersist()
+{   
+    $this->createdAt = new DateTime();
+    
+}
+
+/** @ORM\PreUpdate */
+public function preUpdate()
+{
+    $this->updatedAt = new DateTime();
+  
+}
+
+
+public function getAddress(): ?Address
+{
+    return $this->address;
+}
+
+public function setAddress(?Address $address): self
+{
+    $this->address = $address;
+
+    return $this;
+}
+
+public function getAnimal(): ?Animal
+{
+    return $this->animal;
+}
+
+public function setAnimal(?Animal $animal): self
+{
+    $this->animal = $animal;
+
+    return $this;
+}
+public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+    
+}
---
+++ src/Repository/FoundRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Found;
+use App\Entity\Animal;
+use App\Entity\User;
+use Doctrine\ORM\Query\AST\Join;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Found|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Found|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Found[]    findAll()
+ * @method Found[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class FoundRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Found::class);
+    }
+
+  
+
+    /*
+    public function findOneBySomeField($value): ?Found
+    {
+        return $this->createQueryBuilder('f')
+            ->andWhere('f.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+
+    /**
+     * @return Found[] Returns an array of Found objects
+     */
+    public function findPaged($offset, $size)
+    {
+        
+        return $this->createQueryBuilder('a')
+            ->orderBy('a.id', 'DESC')
+            ->setFirstResult($offset)
+            ->setMaxResults($size)
+            ->getQuery()
+            ->getResult();
+    }
+}
